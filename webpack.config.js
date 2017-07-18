53fdd395f44a2923b98e7ece1db401a9f3163321
xtwang <xtwang@thoughtworks.com>
2017-07-19 00:13:56 +0800

    add image loader to webpack

--- webpack.config.js
+++ webpack.config.js
@@ -14,6 +14,13 @@ module.exports = {
       {
         test: /\.html$/,
         loader: "raw-loader"
+      },
+      {
+        test: /\.(jpe?g|png|gif|svg)$/i,
+        loaders: [
+          'file-loader?hash=sha512&digest=hex&name=[hash].[ext]',
+          'image-webpack-loader?bypassOnDebug&optimizationLevel=7&interlaced=false'
+        ]
       }
     ]
   },
